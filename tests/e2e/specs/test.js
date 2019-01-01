f7fb05207eaf85130ecd05bfff9b1c0765ea7681
Juanjo Molinero <jjmolinero@gmail.com>
2019-01-01 20:24:37 +0100

    [REFACTOR] Extract acceptance tests actions to functions to improve readability

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,43 +1,44 @@
+function enterEmployerOfferOf(amount) {
+  cy.get('.tab').contains('Employer').click();
+  cy.get('input').filter(':visible').type(amount);
+  cy.get('button').filter(':visible').contains('Submit').click();
+}
+
+function enterEmployeeSalaryOf(amount) {
+  cy.get('.tab').contains('Employee').click();
+  cy.get('input').filter(':visible').type(amount);
+  cy.get('button').filter(':visible').contains('Submit').click();
+}
+
+function assertThatResultIs(message) {
+  cy.get('.result').should('contain', message);
+}
+
 describe('Negotiation app', () => {
   it('should show success! if the employee number is less than the employer number', () => {
     cy.visit('/');
 
-    cy.get('.tab').contains('Employer').click();
-    cy.get('input').filter(':visible').type('100');
-    cy.get('button').filter(':visible').contains('Submit').click();
+    enterEmployerOfferOf('100');
+    enterEmployeeSalaryOf('90');
 
-    cy.get('.tab').contains('Employee').click();
-    cy.get('input').filter(':visible').type('90');
-    cy.get('button').filter(':visible').contains('Submit').click();
-
-    cy.get('.result').should('contain', 'Success!');
+    assertThatResultIs('Success!');
   });
 
   it('should show success! if the employee number is equal to the employer number', () => {
     cy.visit('/');
 
-    cy.get('.tab').contains('Employer').click();
-    cy.get('input').filter(':visible').type('100');
-    cy.get('button').filter(':visible').contains('Submit').click();
+    enterEmployerOfferOf('100');
+    enterEmployeeSalaryOf('100');
 
-    cy.get('.tab').contains('Employee').click();
-    cy.get('input').filter(':visible').type('100');
-    cy.get('button').filter(':visible').contains('Submit').click();
-
-    cy.get('.result').should('contain', 'Success!');
+    assertThatResultIs('Success!');
   });
 
   it('should show failure! if the employee number is > the employer number', () => {
     cy.visit('/');
 
-    cy.get('.tab').contains('Employer').click();
-    cy.get('input').filter(':visible').type('90');
-    cy.get('button').filter(':visible').contains('Submit').click();
+    enterEmployerOfferOf('90');
+    enterEmployeeSalaryOf('100');
 
-    cy.get('.tab').contains('Employee').click();
-    cy.get('input').filter(':visible').type('100');
-    cy.get('button').filter(':visible').contains('Submit').click();
-
-    cy.get('.result').should('contain', 'Failure!');
+    assertThatResultIs('Failure!');
   });
 });
